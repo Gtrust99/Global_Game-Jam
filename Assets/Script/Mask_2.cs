fc9e4e0202ce8cb5c0be72b6455f52c8372dc30d
Gtrust99 <reyegiancarlo@gmail.com>
2026-02-01 14:00:59 +0100

    no

--- Assets/Script/Mask_2.cs
+++ Assets/Script/Mask_2.cs
@@ -5,10 +5,9 @@ public class Mask_2 : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
   
-            
             MaskController.Instance.Mask_2 = true;
             MaskController.Instance.UnlockMask_2 = true;
-           
+
         
         gameObject.SetActive(false);
   
