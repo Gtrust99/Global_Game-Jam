d708b73ea80eaa3d6b82f2535d797b4d3167bdf8
Gtrust99 <reyegiancarlo@gmail.com>
2026-02-01 06:57:04 +0100

    mask update

--- Assets/Script/MaskController.cs
+++ Assets/Script/MaskController.cs
@@ -5,36 +5,36 @@ public class MaskController : MonoSingleton<LevelController>
     public bool Mask_1;
     public bool Mask_2;
     public bool Mask_3;
-    //public GameObject Mask1;
-    //public GameObject Mask2;
-    //public GameObject Mask3;
+    public GameObject Mask1;
+    public GameObject Mask2;
+    public GameObject Mask3;
     // Update is called once per frame
     void Update()
     {
-        if (LevelController.Instance.Level_1 && Input.GetKeyDown(KeyCode.A)) //PickMask1
+        if ( Input.GetKeyDown(KeyCode.A)) //PickMask1
         {
             Mask_1 = !Mask_1;
         }
-        if (LevelController.Instance.Level_2 && Input.GetKeyDown(KeyCode.S)) //PickMask2
+        if ( Input.GetKeyDown(KeyCode.S)) //PickMask2
         {
             Mask_2 = !Mask_2;
         }
-        if (LevelController.Instance.Level_3 && Input.GetKeyDown(KeyCode.D)) //PickMask3
+        if ( Input.GetKeyDown(KeyCode.D)) //PickMask3
         {
             Mask_3 = !Mask_3;
         }
         if (Mask_1)
         {
-            
-            //GameObject.SetActive(true);
+
+            Mask1.SetActive(true);
         }
         if (Mask_2)
         {
-            //GameObject.SetActive(true);
+            Mask2.SetActive(true);
         }
         if (Mask_3)
         {
-            //GameObject.SetActive(true);
+            Mask3.SetActive(true);
         }
 
 
