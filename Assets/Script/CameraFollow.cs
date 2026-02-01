a520e55a613221f40b6cf22167a1e958e019abfd
Gtrust99 <reyegiancarlo@gmail.com>
2026-02-01 03:31:52 +0100

    add colliders

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform target; 
+    public float smoothing = 0.125f; 
+    public Vector3 offset; 
 
-        public Transform Player; 
-        public Vector3 offset; 
-
-        void LateUpdate()
-        {
-            transform.position = Player.position + offset;
-        }
-    
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(target.position.x + offset.x, transform.position.y, transform.position.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothing);
+        transform.position = smoothedPosition;
+    }
 }
